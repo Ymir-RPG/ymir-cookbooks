62fb12131489c876a2ff4a021b841f2dffed1213
Skyler Berg <skylertheberg@gmail.com>
2015-11-27 15:32:17 -0800

    Experiment with the backend recipe
    I am trying to make this cookbook at least deploy the application with a
    running development server. This is absolutely not production ready, but
    is a starting point for learning chef and using AWS opsworks.

--- ymir/recipes/default.rb
+++ ymir/recipes/default.rb
@@ -1,6 +1,26 @@
 # Cookbook Name:: ymir
 # Recipe:: default
 
+package 'git' do
+  action :install
+end
+
 package 'python-pip' do
   action :install
+end
+
+git "/ymir-api" do
+  repository "https://github.com/Ymir-RPG/ymir-api.git"
+  reference "master"
+  action :sync
+  user "root"
+end
+
+execute "install_python_dependencies" do
+  command "pip install -r /ymir-api/requirements.txt"
+end
+
+# This is just for testing
+execute "run_server" do
+  command "nohup python /ymir-api/run.py &"
 end
